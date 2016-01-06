d40ef1a3dc1e6b1347ab61b201a93dbd6cfefd57
Roozbeh Matloobi <roozbeh.matloobi@gmail.com>
2016-01-05 20:05:05 -0700

    zend model Acl roles

---
+++ models/AclRoles/Rowset.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * AclRoles rowset
+ *
+ * @category     App
+ * @package      App_Models
+ * @copyright    $Copyright$
+ * @version      $Revision: 12480 $
+ */
+
+/**
+ * Include need classes
+ */
+require_once 'Base/Db/Table/Rowset.php';
+
+/**
+ * AclRoles rowset class
+ *
+ * @category App
+ * @package  App_Models
+ */
+class AclRoles_Rowset extends Base_Db_Table_Rowset {
+}
