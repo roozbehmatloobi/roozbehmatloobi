bec25f8fc981bc0cda0f7ea466fa85573c5cf5fb
Roozbeh Matloobi <roozbeh.matloobi@gmail.com>
2018-01-05 12:27:00 -0600

    Add files via upload

--- utf-8/index.php
+++ utf-8/index.php
@@ -1,14 +1,29 @@
 
+<link rel="stylesheet" type="text/css" href="layout.css">
 <?php 
-$input = $_POST[“name”];
-$reverse = utf8_strrev($input);
-echo "The same string in reverse order $reverse";
-
-function utf8_strrev($input){
-    $input = "你好吗";
-    preg_match_all('/./us', $input, $str);
-    print_r($str[0]);
-    print_r($str);
-    return join('',array_reverse($str[0]));
+if ($_POST){
+	$name = $_POST['name'];
+	$content = $_POST['content'];
 }
 ?>
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script>
+    $( document ).ready(function() {
+   
+    });
+    </script>
+</head>
+<body>
+<form  method = "POST">
+<label style="padding:5px 30px">Name:</label><input class="lab-name" type="text" name="name"><br/>
+<label>Comments:</label> 
+<textarea class="comment" rows = "5" cols="60" name="content"></textarea>
+<br/> <br/> 
+<input type="submit" value="Post" class="submit"><br/>
+</form>
+<br>
+<?php echo $contentFinal .="<b>" . $name . "</br><br/>".$content."<br/>"; ?>
+</body>
+</html>
